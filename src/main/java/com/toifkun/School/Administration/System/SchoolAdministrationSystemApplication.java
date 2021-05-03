3b021b20c3620e09df777251ea600f0ad5d06669
Muhamad Iffat <iffatlathoif@gmail.com>
2021-05-04 06:24:48 +0700

    first commit

---
+++ src/main/java/com/toifkun/School/Administration/System/SchoolAdministrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.toifkun.School.Administration.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAdministrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAdministrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toifkun/School/Administration/System/SchoolAdministrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toifkun.School.Administration.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAdministrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
